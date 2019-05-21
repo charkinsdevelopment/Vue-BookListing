78e5f5fded462d08d4af2f385d86864ee2773cc2
Cory Harkins <cory.harkins@gmail.com>
2019-05-21 13:24:47 -0400

    Create bookitem.vue

---
+++ src/components/BookItem.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+	<li>{{book.title}}:{{book.author}}</li>
+</template>
+
+<script>
+export default {
+	name: 'BookItem',
+	data() {
+		return{
+			props: ['book']
+		}
+	}
+}
+</script>
+
+<style>
+li {display:block; margin:0 10px;}
+</style>
